a1e06f5cf72198aa3c142d2d76f949b22b167906
rob <roby.pakalns@gmail.com>
2025-06-16 21:30:03 +0300

    voxiom routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,39 @@ import { voxiom } from "./routes/voxiom"
 export class TrickoAPI {
     public voxiom = {
         getPlayer: (name: string) => voxiom.player(name),
-        getClan: (name: string) => voxiom.clan(name)
+        getClan: (name: string) => voxiom.clan(name),
+        getMatchBR: (id: string) => voxiom.match_br(id),
+        getMatchCTG: (id: string) => voxiom.match_ctg(id),
+        getSkin: (id: number) => voxiom.skin(id),
+        getLeaderboard: voxiom.leaderboard
     }
 }
+
+
+// Test
+const main = async () => {
+    const api = new TrickoAPI
+
+    // voxiom
+
+    // player
+    // const data = await api.voxiom.getPlayer("acb")
+    // clan
+    // const data = await api.voxiom.getClan("iv")
+    // br match
+    // const data = await api.voxiom.getMatchBR("1df39bf7-8e53-46fa-b1ab-f2ea8598f181")
+    // br match
+    // const data = await api.voxiom.getMatchCTG("7b9f1e10-f471-4a71-aade-b4927f26d854")
+    // skin
+    // const data = await api.voxiom.getSkin(101011010)
+    // leaderboard
+    const data = await api.voxiom.getLeaderboard({
+        type: "ctg",
+        range: "week",
+        sort: "total_score"
+    })
+
+    console.log(data)
+}
+
+main()
--- src/routes/voxiom.ts
+++ src/routes/voxiom.ts
@@ -10,7 +10,54 @@ const voxiomClan = (name: string): FetchResult => {
     return fetchData("voxiom", "clan", name)
 }
 
+const voxiomMatchBR = (id: string): FetchResult => {
+    return fetchData("voxiom", "match/br", id)
+}
+const voxiomMatchCTG = (id: string): FetchResult => {
+    return fetchData("voxiom", "match/ctg", id)
+}
+
+const voxiomSkin = (id: number): FetchResult => {
+    if (id < 1 || id > 531) return badResponse("voxiom", "skin")
+    return fetchData("voxiom", "market/skin", id)
+}
+
+const ranges = ["day", "week", "all"]
+const sorts = {
+    all: ["score", "level"],
+    ctg: ["total_score", "total_games_won", "total_kills", "total_captures"],
+    br: ["total_score", "total_games_won", "total_kills", "total_survival_time"],
+    clan: ["total_score", "total_games_won", "total_kills", "total_power"]
+}
+
+type LeaderboardParameters = { type: string; range: string; sort: string }
+const voxiomLeaderboard = ({ type, range, sort }: LeaderboardParameters): FetchResult => {
+    const _type = type.trim().toLowerCase()
+    const _range = range.trim().toLowerCase()
+    const _sort = sort.trim().toLowerCase()
+
+    const typeKey = _type === "clan" ? "clan" : (_type || "all")
+    const validSorts = sorts[typeKey as keyof typeof sorts]
+
+    const params = new URLSearchParams()
+    if (_type && !["all", "clan"].includes(_type)) params.append("type", _type)
+    if (_range) params.append("range", _range)
+    if (_sort) params.append("sort", _sort)
+
+    if (
+        validSorts &&
+        (typeKey === "all" || ranges.includes(params.get("range") || "")) &&
+        validSorts.includes(params.get("sort") || "")
+    ) return badResponse("voxiom", "leaderboard")
+
+    return fetchData("voxiom", "leaderboard", `?${params.toString()}`)
+}
+
 export const voxiom = {
     player: (name: string) => voxiomPlayer(name),
     clan: (name: string) => voxiomClan(name),
+    match_br: (id: string) => voxiomMatchBR(id),
+    match_ctg: (id: string) => voxiomMatchCTG(id),
+    skin: (id: number) => voxiomSkin(id),
+    leaderboard: voxiomLeaderboard
 }
